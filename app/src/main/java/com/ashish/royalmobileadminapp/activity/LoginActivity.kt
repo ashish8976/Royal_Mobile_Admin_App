10a48b900842f6deebbe9f8a793338afc985cca7
Ashish Rajput <ashish8976>
2023-02-08 00:39:39 +0530

    Product Layout Complete

---
+++ app/src/main/java/com/ashish/royalmobileadminapp/activity/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.ashish.royalmobileadminapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ashish.royalmobileadminapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/ashish/royalmobileadminapp/product/AllAccessoriesActivity.kt
@@ -0,0 +1,12 @@
+package com.ashish.royalmobileadminapp.product
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ashish.royalmobileadminapp.R
+
+class AllAccessoriesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_accessories)
+    }
+}
