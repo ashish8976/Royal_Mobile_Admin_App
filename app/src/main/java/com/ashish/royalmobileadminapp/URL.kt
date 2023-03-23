f9d79992f7a685a3971a3bfba86c31c8cce7629c
Ashish Rajput <ashish8976>
2023-03-23 16:49:32 +0530

    product delete and product details is not proper complete

---
+++ app/src/main/java/com/ashish/royalmobileadminapp/URL.kt
@@ -0,0 +1,9 @@
+package com.ashish.royalmobileadminapp
+
+object URL
+{
+    const val BASE_URL = "http://192.168.1.7:8007"
+    const val IMAGE_PATH = "$BASE_URL/storage/images/"
+}
+
+//http://192.168.1.4:8007
